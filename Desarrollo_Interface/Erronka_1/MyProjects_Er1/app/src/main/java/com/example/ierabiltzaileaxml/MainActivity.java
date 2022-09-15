d12b3fc128143762ace05701b9060fe71337b4ed
Adrian1Arceredillo <96474043+Adrian1Arceredillo@users.noreply.github.com>
2022-09-15 10:28:00 +0200

    .

---
+++ Desarrollo_Interface/Erronka_1/MyProjects_Er1/app/src/main/java/com/example/ierabiltzaileaxml/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ierabiltzaileaxml;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.text.CollationElementIterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
