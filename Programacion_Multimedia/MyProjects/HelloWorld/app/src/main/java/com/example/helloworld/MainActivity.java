188a76eafe9660668a9517631078c5d50a497971
Adrian1Arceredillo <96474043+Adrian1Arceredillo@users.noreply.github.com>
2022-09-16 12:54:51 +0200

    .

--- Programacion_Multimedia/MyProjects/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Programacion_Multimedia/MyProjects/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Hola", "Adrian");
+        Log.d("MainActivity", "Hello World Debug message");
+        Log.i("MainActivity", "Hello World Info message");
+        Log.w("MainActivity", "Hello World Warning message");
+        Log.e("MainActivity", "Hello World Error message");
+        Log.v("MainActivity", "Hello World Verbose message");
+
     }
 }
